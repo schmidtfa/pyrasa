26974bbb169dc3c92f948fa207cf64fb0ea6ec24
Thomas Hartmann <thomas.hartmann@th-ht.de>
2024-07-25 13:50:28 +0200

    format fixes

--- paper/notebooks/check_basic_functionality.py
+++ paper/notebooks/check_basic_functionality.py
@@ -1,20 +1,19 @@
-#%%
+# %%
+import matplotlib.pyplot as plt
+import scipy.signal as dsp
+import seaborn as sns
 from neurodsp.sim import sim_combined
 from neurodsp.utils import create_times
-import scipy.signal as dsp
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 sns.set_style('ticks')
 sns.set_context('poster')
 # %%
 fs = 500
 n_seconds = 60
-duration=2
-overlap=0.5
+duration = 2
+overlap = 0.5
 
-sim_components = {'sim_powerlaw': {'exponent' : -1}, 
-                  'sim_oscillation': {'freq' : 10}}
+sim_components = {'sim_powerlaw': {'exponent': -1}, 'sim_oscillation': {'freq': 10}}
 # %%
 
 sig = sim_combined(n_seconds=n_seconds, fs=fs, components=sim_components)
@@ -25,7 +24,7 @@ f, axes = plt.subplots(ncols=2, figsize=(8, 4))
 axes[0].plot(times[max_times], sig[max_times])
 axes[0].set_ylabel('Amplitude (a.u.)')
 axes[0].set_xlabel('Time (s)')
-freq, psd = dsp.welch(sig, fs=fs, nperseg=duration*fs, noverlap=duration*fs*overlap)
+freq, psd = dsp.welch(sig, fs=fs, nperseg=duration * fs, noverlap=duration * fs * overlap)
 axes[1].loglog(freq, psd)
 axes[1].set_ylabel('Power (a.u.)')
 axes[1].set_xlabel('Frequency (Hz)')
@@ -34,42 +33,43 @@ plt.tight_layout()
 
 
 from pyrasa.irasa import irasa
+
 # %%
-freq_irasa, psd_ap, psd_p = irasa(sig, 
-                                  fs=fs, 
-                                  band=(1, 150), 
-                                  kwargs_psd={'nperseg': duration*fs, 
-                                             'noverlap': duration*fs*overlap
-                                            },
-                                hset_info=(1, 2, 0.05))
+freq_irasa, psd_ap, psd_p = irasa(
+    sig,
+    fs=fs,
+    band=(1, 150),
+    kwargs_psd={'nperseg': duration * fs, 'noverlap': duration * fs * overlap},
+    hset_info=(1, 2, 0.05),
+)
 # %%
 f, axes = plt.subplots(ncols=2, figsize=(8, 4))
 axes[0].set_title('Periodic')
-axes[0].plot(freq_irasa, psd_p[0,:])
+axes[0].plot(freq_irasa, psd_p[0, :])
 axes[0].set_ylabel('Power (a.u.)')
 axes[0].set_xlabel('Frequency (Hz)')
 axes[1].set_title('Aperiodic')
-axes[1].loglog(freq_irasa, psd_ap[0,:])
+axes[1].loglog(freq_irasa, psd_ap[0, :])
 axes[1].set_ylabel('Power (a.u.)')
 axes[1].set_xlabel('Frequency (Hz)')
 
 f.tight_layout()
 # %% get periodic stuff
 from pyrasa.utils.peak_utils import get_peak_params
+
 get_peak_params(psd_p, freqs=freq_irasa)
 # %% get aperiodic stuff
 from pyrasa.utils.aperiodic_utils import compute_slope
-ap_params, gof_params = compute_slope(aperiodic_spectrum=psd_ap,
-                                       freqs=freq_irasa, fit_func='knee')
+
+ap_params, gof_params = compute_slope(aperiodic_spectrum=psd_ap, freqs=freq_irasa, fit_func='knee')
 ap_params
 # %%
 gof_params
 # %% Lets check the knee
 knee_freq = 15
-knee = knee_freq ** (2*.5 + 1.5)
+knee = knee_freq ** (2 * 0.5 + 1.5)
 
-sim_components = {'sim_knee': {'exponent1' : -.5, 'exponent2': -1.5, 'knee':knee }, 
-                  'sim_oscillation': {'freq' : 10}}
+sim_components = {'sim_knee': {'exponent1': -0.5, 'exponent2': -1.5, 'knee': knee}, 'sim_oscillation': {'freq': 10}}
 
 sig = sim_combined(n_seconds=n_seconds, fs=fs, components=sim_components)
 # %%
@@ -78,28 +78,28 @@ f, axes = plt.subplots(ncols=2, figsize=(8, 4))
 axes[0].plot(times[max_times], sig[max_times])
 axes[0].set_ylabel('Amplitude (a.u.)')
 axes[0].set_xlabel('Time (s)')
-freq, psd = dsp.welch(sig, fs=fs, nperseg=duration*fs, noverlap=duration*fs*overlap)
+freq, psd = dsp.welch(sig, fs=fs, nperseg=duration * fs, noverlap=duration * fs * overlap)
 axes[1].loglog(freq, psd)
 axes[1].set_ylabel('Power (a.u.)')
 axes[1].set_xlabel('Frequency (Hz)')
 
 plt.tight_layout()
 # %%
-freq_irasa, psd_ap, psd_p = irasa(sig, 
-                                  fs=fs, 
-                                  band=(1, 150), 
-                                  kwargs_psd={'nperseg': duration*fs, 
-                                             'noverlap': duration*fs*overlap
-                                            },
-                                hset_info=(1, 2, 0.025))
+freq_irasa, psd_ap, psd_p = irasa(
+    sig,
+    fs=fs,
+    band=(1, 150),
+    kwargs_psd={'nperseg': duration * fs, 'noverlap': duration * fs * overlap},
+    hset_info=(1, 2, 0.025),
+)
 # %%
 f, axes = plt.subplots(ncols=2, figsize=(8, 4))
 axes[0].set_title('Periodic')
-axes[0].plot(freq_irasa, psd_p[0,:])
+axes[0].plot(freq_irasa, psd_p[0, :])
 axes[0].set_ylabel('Power (a.u.)')
 axes[0].set_xlabel('Frequency (Hz)')
 axes[1].set_title('Aperiodic')
-axes[1].loglog(freq_irasa, psd_ap[0,:])
+axes[1].loglog(freq_irasa, psd_ap[0, :])
 axes[1].set_ylabel('Power (a.u.)')
 axes[1].set_xlabel('Frequency (Hz)')
 
@@ -107,9 +107,11 @@ f.tight_layout()
 
 # %% get periodic stuff
 from pyrasa.utils.peak_utils import get_peak_params
+
 get_peak_params(psd_p, freqs=freq_irasa)
 # %% get aperiodic stuff
 from pyrasa.utils.aperiodic_utils import compute_slope
+
 ap_params, gof_params = compute_slope(aperiodic_spectrum=psd_ap, freqs=freq_irasa, fit_func='fixed')
 ap_params
 # %%
@@ -117,6 +119,6 @@ gof_params
 # %%
 ap_params, gof_params = compute_slope(aperiodic_spectrum=psd_ap, freqs=freq_irasa, fit_func='knee')
 ap_params
-# %% 
+# %%
 gof_params
 # %%
--- paper/notebooks/check_irasa_mne.py
+++ paper/notebooks/check_irasa_mne.py
@@ -1,8 +1,9 @@
-#%%
+# %%
 import sys
-from neurodsp.sim import sim_combined
+
 import numpy as np
 import seaborn as sns
+from neurodsp.sim import sim_combined
 
 sns.set_style('ticks')
 sns.set_context('paper')
@@ -10,70 +11,79 @@ sys.path.append('../')
 
 
 import matplotlib as mpl
-new_rc_params = {'text.usetex': False,
-                 "svg.fonttype": 'none'
-}
+
+new_rc_params = {'text.usetex': False, 'svg.fonttype': 'none'}
 mpl.rcParams.update(new_rc_params)
 
 import mne
 from mne.datasets import sample
 
+from pyrasa.irasa_mne import irasa_epochs, irasa_raw
 
-
-from pyrasa.irasa_mne import irasa_raw, irasa_epochs
-
-#%% now lets check the mne python implementation
+# %% now lets check the mne python implementation
 
 
 data_path = sample.data_path()
-subjects_dir = data_path / "subjects"
-subject = "sample"
+subjects_dir = data_path / 'subjects'
+subject = 'sample'
 
-meg_path = data_path / "MEG" / "sample"
-raw_fname = meg_path / "sample_audvis_raw.fif"
-fwd_fname = meg_path / "sample_audvis-meg-eeg-oct-6-fwd.fif"
+meg_path = data_path / 'MEG' / 'sample'
+raw_fname = meg_path / 'sample_audvis_raw.fif'
+fwd_fname = meg_path / 'sample_audvis-meg-eeg-oct-6-fwd.fif'
 fwd = mne.read_forward_solution(fwd_fname)
-src = fwd["src"]
+src = fwd['src']
 
 
 raw = mne.io.read_raw_fif(raw_fname)
-picks = mne.pick_types(raw.info, meg='mag', eeg=False, 
-                       stim=False, eog=False, exclude="bads")
+picks = mne.pick_types(raw.info, meg='mag', eeg=False, stim=False, eog=False, exclude='bads')
 raw.pick(picks)
 
-#%% lets simulate 2 different signals
+# %% lets simulate 2 different signals
 info = raw.info.copy()
-tstep = 1.0 / info["sfreq"]
+tstep = 1.0 / info['sfreq']
 
-alpha = sim_combined(60, 
-                     info["sfreq"], 
-                     components={'sim_oscillation': {'freq' : 10},
-                                 'sim_powerlaw': {'exponent': -1.5,}})
+alpha = sim_combined(
+    60,
+    info['sfreq'],
+    components={
+        'sim_oscillation': {'freq': 10},
+        'sim_powerlaw': {
+            'exponent': -1.5,
+        },
+    },
+)
 
-beta = sim_combined(60, 
-                     info["sfreq"], 
-                     components={'sim_oscillation': {'freq' : 20},
-                                 'sim_powerlaw': {'exponent': -1.5,}})
+beta = sim_combined(
+    60,
+    info['sfreq'],
+    components={
+        'sim_oscillation': {'freq': 20},
+        'sim_powerlaw': {
+            'exponent': -1.5,
+        },
+    },
+)
 
 
-#%% now lets simulate some data
-#TODO: put in a helper function
+# %% now lets simulate some data
+# TODO: put in a helper function
+
 
 def simulate_raw_(signal, scaling_fator, region, subject, info, subjects_dir):
-    
-    '''Shorthand function to simulate a dipole'''
+    """Shorthand function to simulate a dipole"""
 
     selected_label = mne.read_labels_from_annot(
-        subject, regexp=region, subjects_dir=subjects_dir,
+        subject,
+        regexp=region,
+        subjects_dir=subjects_dir,
     )[0]
-    location = "center"  # Use the center of the region as a seed.
+    location = 'center'  # Use the center of the region as a seed.
     extent = 10.0  # Extent in mm of the region.
     label = mne.label.select_sources(
-        subject, selected_label, location=location, 
-        extent=extent, subjects_dir=subjects_dir
+        subject, selected_label, location=location, extent=extent, subjects_dir=subjects_dir
     )
 
-    #this is the source ts
+    # this is the source ts
     source_time_series = signal * scaling_fator
 
     n_events = 1
@@ -85,40 +95,35 @@ def simulate_raw_(signal, scaling_fator, region, subject, info, subjects_dir):
     source_simulator.add_data(label, source_time_series, events)
 
     raw = mne.simulation.simulate_raw(info, source_simulator, forward=fwd)
-    #cov = mne.make_ad_hoc_cov(raw.info)
-    #mne.simulation.add_noise(raw, cov, iir_filter=[0.2, -0.2, 0.04])
+    # cov = mne.make_ad_hoc_cov(raw.info)
+    # mne.simulation.add_noise(raw, cov, iir_filter=[0.2, -0.2, 0.04])
 
     return raw
 
-#%% simulate alpha + beta and combine
-raw_alpha = simulate_raw_(alpha, 5e-9, "caudalmiddlefrontal-rh", 
-                          subject, info, subjects_dir)
 
-raw_beta = simulate_raw_(beta, 10e-10, "caudalmiddlefrontal-lh", 
-                          subject, info, subjects_dir)
+# %% simulate alpha + beta and combine
+raw_alpha = simulate_raw_(alpha, 5e-9, 'caudalmiddlefrontal-rh', subject, info, subjects_dir)
+
+raw_beta = simulate_raw_(beta, 10e-10, 'caudalmiddlefrontal-lh', subject, info, subjects_dir)
 
 raw_data = raw_alpha.get_data() + raw_beta.get_data()
 
 raw = mne.io.RawArray(raw_data, info)
 
-picks = mne.pick_types(raw.info, meg='mag', eeg=False, 
-                       stim=False, eog=False, exclude="bads")
+picks = mne.pick_types(raw.info, meg='mag', eeg=False, stim=False, eog=False, exclude='bads')
 raw.pick(picks)
 
-#%%
-aperiodic_mne, periodic_mne = irasa_raw(raw, band=(.25, 50), 
-                                duration=2, 
-                                hset_info=(1.,2.,.05),
-                                as_array=False)
-#%%
+# %%
+aperiodic_mne, periodic_mne = irasa_raw(raw, band=(0.25, 50), duration=2, hset_info=(1.0, 2.0, 0.05), as_array=False)
+# %%
 df_slopes, df_gof = aperiodic_mne.get_slopes()
 
-#%%
+# %%
 df_slopes.head()
-#%%
+# %%
 df_gof.head()
 
-#%%
+# %%
 
 peak_df = periodic_mne.get_peaks()
 
@@ -126,18 +131,18 @@ peak_df.head()
 
 periodic_mne.plot()
 
-#%% now lets check-out the events
+# %% now lets check-out the events
 event_id = {
-    "Auditory/Left": 1,
-    "Auditory/Right": 2,
-    "Visual/Left": 3,
-    "Visual/Right": 4,
+    'Auditory/Left': 1,
+    'Auditory/Right': 2,
+    'Visual/Left': 3,
+    'Visual/Right': 4,
 }
 tmin = -0.2
 tmax = 0.5
 
 # Load real data as the template
-event_fname = meg_path / "sample_audvis_filt-0-40_raw-eve.fif"
+event_fname = meg_path / 'sample_audvis_filt-0-40_raw-eve.fif'
 events = mne.read_events(event_fname)
 
 
@@ -147,43 +152,39 @@ epochs = mne.Epochs(
     event_id,
     tmin,
     tmax,
-    #picks=picks,
+    # picks=picks,
     baseline=None,
     preload=True,
     verbose=False,
 )
 
-#%%
+# %%
 
-aperiodic, periodic = irasa_epochs(epochs, 
-                                   band=(.5, 50), 
-                                   hset_info=(1.,2.,.05), 
-                                   as_array=False)
+aperiodic, periodic = irasa_epochs(epochs, band=(0.5, 50), hset_info=(1.0, 2.0, 0.05), as_array=False)
 
-#%%
+# %%
 periodic.get_peaks(polyorder=0)
 
-#%%
+# %%
 aperiodic.get_slopes()
 
-#%%
-periodic["Auditory/Left"].average().plot_topomap(dB=False)
-#%%
-periodic["Auditory/Right"].average().plot_topomap(dB=False)
-#%%
-periodic["Visual/Left"].average().plot_topomap(dB=False)
-#%%
-periodic["Visual/Right"].average().plot_topomap(dB=False)
+# %%
+periodic['Auditory/Left'].average().plot_topomap(dB=False)
+# %%
+periodic['Auditory/Right'].average().plot_topomap(dB=False)
+# %%
+periodic['Visual/Left'].average().plot_topomap(dB=False)
+# %%
+periodic['Visual/Right'].average().plot_topomap(dB=False)
 
 
-
-#%%
-aperiodic["Auditory/Left"].average().plot_topomap(dB=False)
-#%%
-aperiodic["Auditory/Right"].average().plot_topomap(dB=False)
-#%%
-aperiodic["Visual/Left"].average().plot_topomap(dB=False)
-#%%
-aperiodic["Visual/Right"].average().plot_topomap(dB=False)
+# %%
+aperiodic['Auditory/Left'].average().plot_topomap(dB=False)
+# %%
+aperiodic['Auditory/Right'].average().plot_topomap(dB=False)
+# %%
+aperiodic['Visual/Left'].average().plot_topomap(dB=False)
+# %%
+aperiodic['Visual/Right'].average().plot_topomap(dB=False)
 
 # %%
