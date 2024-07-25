e2dc85559f4b0b83a18c13566d6c0983f951c1c0
Thomas Hartmann <thomas.hartmann@th-ht.de>
2024-07-25 13:52:00 +0200

    remove obsolete TEST_PATH

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,4 @@
 # %%
-from pathlib import Path
 
 import numpy as np
 
@@ -12,9 +11,3 @@ KNEE_FREQ = [10, 20, 30]
 TOLERANCE = 0.1
 MIN_R2 = 0.8  # seems like a sensible minimum
 MIN_CORR_PSD_CMB = 0.99
-
-
-# Path Settings
-TESTS_PATH = Path(__file__).parent.absolute()
-
-# %%
