4fb115655c460cd9b662838f7614393b06d6302b
Thomas Hartmann <thomas.hartmann@th-ht.de>
2024-07-30 15:33:18 +0200

    remove obsolete IrasaKwargsTyped

--- pyrasa/utils/input_classes.py
+++ pyrasa/utils/input_classes.py
@@ -1,18 +1,5 @@
 from typing import TypedDict
 
-import numpy as np
-
-
-class IrasaKwargsTyped(TypedDict):
-    nperseg: int | None
-    noverlap: int | None
-    nfft: int | None
-    h: int | None
-    time_orig: None | np.ndarray
-    up_down: str | None
-    dpss_settings: dict
-    win_kwargs: dict
-
 
 class IrasaSprintKwargsTyped(TypedDict):
     mfft: int
