a267701efa989655129804f86f175d566dc42cd7
Schmidt Fabian <fabian.schmidt@psy25331.wlan.sbg.ac.at>
2024-07-31 14:56:40 +0200

    added custom slope fitting

--- tests/test_mne.py
+++ tests/test_mne.py
@@ -1,24 +1,24 @@
-# import pytest
-# from neurodsp.utils.sim import set_random_seed
+import pytest
+from neurodsp.utils.sim import set_random_seed
 
-# from pyrasa.irasa_mne import irasa_epochs, irasa_raw
+from pyrasa.irasa_mne import irasa_epochs, irasa_raw
 
-# set_random_seed(42)
+set_random_seed(42)
 
 
-# @pytest.mark.filterwarnings('ignore:RuntimeWarning')
-# @pytest.mark.filterwarnings('ignore:UserWarning')
-# def test_mne(gen_mne_data_raw):
-#     mne_data, epochs = gen_mne_data_raw
+@pytest.mark.filterwarnings('ignore:RuntimeWarning')
+@pytest.mark.filterwarnings('ignore:UserWarning')
+def test_mne(gen_mne_data_raw):
+    mne_data, epochs = gen_mne_data_raw
 
-#     # test raw
-#     aperiodic_mne, periodic_mne = irasa_raw(
-#         mne_data, band=(0.25, 50), duration=2, hset_info=(1.0, 2.0, 0.05), as_array=False
-#     )
-#     aperiodic_mne.get_slopes(fit_func='fixed')
-#     periodic_mne.get_peaks(smoothing_window=2)
+    # test raw
+    aperiodic_mne, periodic_mne = irasa_raw(
+        mne_data, band=(0.25, 50), duration=2, hset_info=(1.0, 2.0, 0.05), as_array=False
+    )
+    aperiodic_mne.get_slopes(fit_func='fixed')
+    periodic_mne.get_peaks(smoothing_window=2)
 
-#     # test epochs
-#     aperiodic, periodic = irasa_epochs(epochs, band=(0.5, 50), hset_info=(1.0, 2.0, 0.05), as_array=False)
-#     aperiodic.get_slopes(fit_func='fixed', scale=True)
-#     periodic.get_peaks(smoothing_window=2)
+    # test epochs
+    aperiodic, periodic = irasa_epochs(epochs, band=(0.5, 50), hset_info=(1.0, 2.0, 0.05), as_array=False)
+    aperiodic.get_slopes(fit_func='fixed', scale=True)
+    periodic.get_peaks(smoothing_window=2)
